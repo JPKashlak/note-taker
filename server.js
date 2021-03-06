1c7a7151d3ae26477b9b6467d1b452fa8571daac
jpkashlak <jpkashlak@gmail.com>
2021-03-06 15:13:29 -0600

    parsed posted data

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const { notes } = require('./Develop/db/db')
 
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
 })
 
 app.listen(3001, () => {
