3dbd7b7a5da7cb8e28dfbca64414e66a0351ba27
jpkashlak <jpkashlak@gmail.com>
2021-03-06 15:31:44 -0600

    tested via insomnia to post data to db.json

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const { notes } = require('./Develop/db/db')
 
+function createNewNote(body, notesArray)  {
+    const note = body
+    notesArray.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    )
+
+    return note;
+}
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body)
-    res.json(req.body)
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes)
+
+    res.json(note)
 })
 
 app.listen(3001, () => {
